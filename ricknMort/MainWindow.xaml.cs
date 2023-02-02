48aec5a26e66dd4162e462674f74f514c6847f9f
Parker Williams <Parker.L.Williams-3@ou.edu>
2023-02-02 16:00:46 -0600

    Add project files.

---
+++ ricknMort/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ricknMort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            HttpClient client = new HttpClient();
+            string json = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+            client.Dispose();
+
+            RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+            cboCharacters.Items.Add(api);
+
+            foreach(ResultItem character in api.results)
+            {
+                cboCharacters.Items.Add(character);
+            }
+        }
+
+        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            ResultItem selectedCharacter = (ResultItem)cboCharacters.SelectedItem;
+
+            txtName.Text = selectedCharacter.name;
+
+            imgPicture.Source = new BitmapImage(new Uri(selectedCharacter.image));
+        }
+    }
+}
